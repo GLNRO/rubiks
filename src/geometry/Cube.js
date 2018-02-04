08a172141cc5a53114b4f48533228dc950739e24
Lauren Gallinaro <lg@glnro.com>
2018-02-04 22:42:45 +0100

    Update Cube initialization parameters

--- src/geometry/Cube.js
+++ src/geometry/Cube.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import * as THREE from 'three';
 
-const Cube = (position) => {
+const Cube = (specs) => {
+  let position = specs.startingCoord;
+  let colors = specs.colors
   let geometry = new THREE.BoxGeometry( .9, .9,.9 );
-  let material = new THREE.MeshPhongMaterial({color: 0x7E57C2,vertexColors: THREE.FaceColors})
+  let material = new THREE.MeshPhongMaterial({color: 0xc0c0c0 ,vertexColors: THREE.FaceColors})
   let cube = new THREE.Mesh( geometry, material );
   cube.position.x = position.x
   cube.position.y = position.y
   cube.position.z = position.z
+  _.forEach(colors, (value,key) => {cube.geometry.faces[key].color.setHex(value)})
   return cube;
 
 }
