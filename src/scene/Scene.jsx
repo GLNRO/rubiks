19acc82b632c45b161a66dd7ae2b9be68ac26f5d
Lauren Gallinaro <lg@glnro.com>
2018-02-04 22:43:11 +0100

    Clean up scene creation

--- src/scene/Scene.jsx
+++ src/scene/Scene.jsx
@@ -5,6 +5,8 @@ let OrbitControls = require('three-orbit-controls')(THREE);
 import BottomFace from '../geometry/BottomFace';
 import CenterFace from '../geometry/CenterFace';
 import FrontFace from '../geometry/FrontFace';
+import CubeFactory from '../services/CubeFactory';
+import * as LightFactory from '../services/LightFactory';
 export default class Scene extends Component{
 
   componentDidMount(){
@@ -19,32 +21,16 @@ export default class Scene extends Component{
         75, width/height,1,1000
       )
       camera.position.z = 7;
-      let light = new THREE.AmbientLight( 0x404040 );
-      let dirLight = new THREE.DirectionalLight( {color: 0xffffff, intensity: .1} );
-      dirLight.position.set( 15, 15, 15 ).normalize();
-      let dirLight2 = new THREE.DirectionalLight( {color: 0xffffff, intensity: .1} );
-      dirLight2.position.set( -15, -15, -15 ).normalize();
-      let dirLight3 = new THREE.DirectionalLight( {color: 0xffffff, intensity: .1} );
-      dirLight3.position.set( -15, 15, -15 ).normalize();
-      let dirLight4 = new THREE.DirectionalLight( {color: 0xffffff, intensity: .1} );
-      dirLight4.position.set( 15, -15, 15 ).normalize();
-      scene.add(light);
-      scene.add(dirLight);
-      scene.add(dirLight2);
-      scene.add(dirLight3);
-      scene.add(dirLight4);
+      let lights = LightFactory.setupSceneLights();
+      console.log(lights)
+      _.forEach(lights, (light) => {scene.add(light)})
       let controls = new OrbitControls(camera)
       const renderer = new THREE.WebGLRenderer({antialias: true})
       renderer.setClearColor('#90A4AE')
       renderer.setSize(width, height)
 
-      let bottom = BottomFace();
-      let center = CenterFace();
-      let front = FrontFace();
-      console.log(bottom)
-      _.forEach(bottom, (value,key) => {scene.add(value)})
-      _.forEach(center, (value,key) => {scene.add(value)})
-      _.forEach(front, (value,key) => {scene.add(value)})
+      let cubeFactory = CubeFactory()
+      _.forEach(cubeFactory, (cube) => {scene.add(cube)})
 
       this.scene = scene
       this.camera = camera
